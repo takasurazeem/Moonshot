14dc9655720230ef78a5c9b0023822e28641b925
Takasur Azeem <takasurazeem@gmail.com>
2022-03-06 22:20:07 +0500

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Takasur Azeem on 06/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
